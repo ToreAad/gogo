e58d3bb68178dfdc5e59f4250d8ed0d2369bf493
Tore Aadland <9218425+ToreAad@users.noreply.github.com>
2021-09-21 20:56:21 +0200

    wip goboard_fast

---
+++ base/goboard_fast.go
@@ -0,0 +1,22 @@
+package base
+
+import "math/rand"
+
+type state struct {
+	point  Point
+	player PlayerColor
+}
+
+func getHashes(rows int, cols int) map[state]int {
+	r := rand.New((rand.NewSource(42)))
+	res := make(map[state]int)
+	for i := 1; i <= rows; i++ {
+		for j := 1; j <= cols; j++ {
+			for p := range []PlayerColor{Black, White} {
+				s := state{Point{i, j}, PlayerColor(p)}
+				res[s] = int(r.Int63())
+
+			}
+		}
+	}
+}
